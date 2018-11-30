a6686d69d249df43921019008a9ef9c562909fde
c15314356 <c15314356@mydit.ie>
2018-11-30 14:17:51 +0000

    Added main HTML, CSS and Javascript Files.
    All these files are required for NGinx

---
+++ scripts/connection.js
@@ -0,0 +1,41 @@
+// This will execute when the page is ready.
+$(function() {
+    
+    $.ajax({
+        url: 'http://127.0.0.1:9000/db',
+        method: 'GET',
+    }).done(function(response){
+        printResponse(response);
+        mapMarkers(response, mymap);
+    });
+
+    // // Create Ajax request (XMLHTTPRequest)
+    // test = $.ajax({
+    //     url: 'http://127.0.0.1:9000/login', // URL TO HOST
+    //     method: 'POST', // METHOD
+    //     // DATA TO SEND
+    //     data: { 
+    //         username: 'test', 
+    //         password: 'hello world'
+    //     }
+    //     // When data received (response == 200)
+    // }).done(function(response){
+    //     console.log(response);
+    //     // When response failed (no internet conenction, connection aborted ect)
+    // }).fail(function(error){
+    //     console.error('Something bad happened.', error);
+    // });
+
+    // Creating intial map, zoom level and location.
+    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+    // Add OpenStreetMap tile layer, so we have an actual map.
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoiYzE1MzE0MzU2IiwiYSI6ImNqb2ZtcmU5ZjA1anAzdnF6cWVtaWUxMG4ifQ.YoM7Ip2CPDpiIsect76L1Q'
+    }).addTo(mymap);
+
+    createMarkers(mymap);
+});
---
+++ scripts/crime_functions.js
@@ -0,0 +1,87 @@
+function createMarkers(mymap){
+    // Add markers.
+    L.marker([51.5, -0.09]).bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup().addTo(mymap);
+    
+    L.marker([51.516703, -0.093558]).bindPopup("<b>Hello world!</b><br>I am a popup.2").openPopup().addTo(mymap);
+}
+
+function printResponse(response){
+    const container = $('#test')[0];
+    for (let i = 0; i < response.length; i++) {
+        const test = response[i];
+        const div = document.createElement( 'div');
+        div.classList.add('row');
+        div.innerHTML = 'Crime: ' + test.crime_type + ' Lat/Long: ' + test.latitude + ',' + test.longitude + ' Loc Details: ' + test.location_details + ' outcome: ' + test.last_outcome;
+        container.appendChild(div);
+    }
+}
+
+function mapMarkers(response, mymap){
+    for (let i = 0; i < response.length; i++) {
+        const crime = response[i];
+        L.marker([crime.latitude, crime.longitude]).bindPopup(crime.location_details + '<br>' + crime.last_outcome).openPopup().addTo(mymap);
+    }
+}
+
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("fullMap").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("fullMap").style.marginLeft = "0";
+}
+
+function openPage(pageName, elmnt) {
+    // Hide all elements with class="tabcontent" by default */
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Remove the background color of all tablinks/buttons
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+
+    // Show the specific tab content
+    document.getElementById(pageName).style.display = "block";
+
+    // Add the specific color to the button used to open the tab content
+    elmnt.style.backgroundColor = "orange";
+}
+
+function loadgraph() {
+    var canvas = document.getElementById('myChart');
+    var data = {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+            {
+                label: "My First dataset",
+                backgroundColor: "rgba(255,99,132,0.2)",
+                borderColor: "rgba(255,99,132,1)",
+                borderWidth: 2,
+                hoverBackgroundColor: "rgba(255,99,132,0.4)",
+                hoverBorderColor: "rgba(255,99,132,1)",
+                data: [65, 59, 30, 81, 56, 55, 40],
+            }
+        ]
+    };
+    var option = {
+        animation: {
+            duration:5000
+    }
+    };
+
+
+    var myBarChart = Chart.Bar(canvas, {
+        data:data,
+        options:option
+    });
+}
+
