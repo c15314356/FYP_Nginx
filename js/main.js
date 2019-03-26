f7038571e397f8553a38358755ef1658365a13fc
c15314356 <c15314356@mydit.ie>
2019-03-26 00:45:18 +0000

    Reformated Web Folder Structure to Web Standards
    Updated root directories to match web application folder conventions.
    
    Fixed spacing with graphs, made it more accessible for phones.
    
    Added save/export option for generated graphs.
    
    Removed uncessary files from third party libraries including leaflet and bootstrap.

--- js/main.js
+++ js/main.js
@@ -1,26 +1,13 @@
 /* GLOBAL VARIABLES */
 // Creating intial map, zoom level and location.
-var MAP = L.map('mapid').setView([51.505, -0.09], 14);
+var MAP = L.map('mapid').setView([52.686373, -1.362305], 8);
 LOCAL_URL = 'http://127.0.0.1:9000';
 EXTERNAL_URL = 'http://51.141.10.255:9000';
 CURRENT_URL = LOCAL_URL;
+GRAPHNUMBER = 0;
 
 /* Calls all functions once page is ready. */
 $(function() {
-
-    /* Retrieves geoghraphical data from database and adds them to initial map. */
-    $.ajax({
-        url: CURRENT_URL + '/db',
-        method: 'GET',
-        withCredentials: true
-    }).done(function(response){
-        console.log(response);
-        // addMarkerTypesThenAddToMap(response);
-        addMarkerCLusterGroupsToMap(response);
-    }).fail(function(error){
-        console.error('Problem occurred when trying to connect to Node Service API.', error);
-    });
-    
     /* Retrieves regional coordinate data from database and creates regions based on them. */
     $.ajax({
         url: CURRENT_URL +'/regions',
@@ -62,24 +49,6 @@ $(function() {
     //go to my current location WORK ON THESE LATER
     // MAP.locate();
 
-    // var geojsonFeature = {
-    //     "type": "Feature",
-    //     "properties": {
-    //         "name": "Coors Field",
-    //         "amenity": "Baseball Stadium",
-    //         "popupContent": "This is where the Rockies play!"
-    //     },
-    //     "geometry": {
-    //         "type": "Polygon",
-    //         "coordinates": [
-    //             [53.8913912806, -5.72387693339],
-    //             [54.438102791, -8.6242675385],
-    //             [53.943154694, -7.22900388672],
-    //             [53.8913912806, -5.72387693339],
-    //     ]
-    //     }
-    // };
-
     // // Geojson.
     // L.geoJSON(geojsonFeature, {
     //     // style: myStyle
@@ -87,17 +56,12 @@ $(function() {
     // console.log('done');
 
     // Map events & functions.
-    var popup = L.popup();
-    function onMapClick(e) {
-        popup.setLatLng(e.latlng).setContent("You clicked the map at " + e.latlng.toString()).openOn(MAP);
-    }
-
-    MAP.on('click', onMapClick);
+    // var popup = L.popup();
+    // function onMapClick(e) {
+    //     popup.setLatLng(e.latlng).setContent("You clicked the map at " + e.latlng.toString()).openOn(MAP);
+    // }
+    // MAP.on('click', onMapClick);
 
     // Open home tab as default on pageload
     document.getElementById("defaultTab").click(); 
-    
-    // Visualises graphical data.
-    loadgraph();
-
 });
